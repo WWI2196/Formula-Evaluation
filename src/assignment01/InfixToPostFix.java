c79d79d3e19a85653d8b86afb038f69c3b89319e
Melkor <127069062+WWI2196@users.noreply.github.com>
2024-10-13 20:46:54 +0530

    Update InfixToPostFix.java

--- src/assignment01/InfixToPostFix.java
+++ src/assignment01/InfixToPostFix.java
@@ -2,64 +2,59 @@ package assignment01;
 import java.util.*;
 
 public class InfixToPostFix {
-    protected static String infixToPostFix(String infix){
+    protected static String convertInFlixToPostFix(String inflix){
         StringBuilder postFix = new StringBuilder();
         Stack<Character> stack = new Stack<>();
         
-        for(char c: infix.toCharArray()){
-            if(c==' '){
+        for (char c:inflix.toCharArray()){
+            if (c == ' '){ // if black space was found simply continue to the next character of the inflix formula 
                 continue;
             }
             
-            if (Character.isDigit(c)||c=='.'){
+            if (Character.isDigit(c) || c == '.'){ // if a number/decimal place is found add it to the string
                 postFix.append(c);
             }
-            else {
-                if ( postFix.length()>0 && postFix.charAt(postFix.length()-1) != ' '){
-                postFix.append(' ');
+            else{
+                if (postFix.length()>0 && postFix.charAt(postFix.length()-1) != ' '){ // if postfix has black space 
+                    postFix.append(' ');
                 }
             }
             
             if (c == '('){
                 stack.push(c);
             }
-            else if(c==')'){
-                while(!stack.isEmpty() && stack.peek() != '('){
-                    postFix.append(stack.pop());
-                }
-                if (!stack.isEmpty()){
-                stack.pop(); // To remove the '(' from the stack
-                }
-
-            }
-            else if(isOperator(c)){
-                while(!stack.isEmpty() && precedence(c) <= precedence(stack.peek())){
+            else if(c == ')'){ // handle for the brackets
+                while (!stack.isEmpty() && stack.peek() !='('){
                     postFix.append(stack.pop());
                 }
                 
-                stack.push(c); 
+                if (!stack.isEmpty()){
+                    stack.pop();
+                } 
+            }
+            else if(isOperator(c)){ // handle the opetors accorinf to the presendence
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())){
+                    postFix.append(stack.pop());
+                }
+                stack.push(c);
             }
         }
         
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()){
             postFix.append(stack.pop());
         }
-       
-        return postFix.toString();
-        
+        return postFix.toString(); 
     }
     
-    private static int precedence(Character operator){
-        return switch (operator) {
-            case '-' -> 1;
-            case '+' -> 2;
-            case '*' -> 3;
-            case '/' -> 4;
+    private static int precedence(Character operator){ // precedence for the operators
+        return switch(operator){
+            case '-','+' -> 1;
+            case '*','/' -> 2;
             default -> -1;
         };
     }
     
     protected static boolean isOperator(Character c){
-        return (c == '-'||c=='+'||c=='*'||c=='/');
+        return (c == '+'||c == '-'|| c == '*'|| c == '/');
     }
 }
