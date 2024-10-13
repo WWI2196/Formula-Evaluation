32d4aa9a7f52193f7e7de365685620a5e17530a8
Melkor <127069062+WWI2196@users.noreply.github.com>
2024-10-13 20:45:33 +0530

    Update ConvertToBrackets.java

--- src/assignment01/ConvertToBrackets.java
+++ src/assignment01/ConvertToBrackets.java
@@ -2,17 +2,16 @@ package assignment01;
 
 
 public class ConvertToBrackets {
-    protected static String convertToNormalBrackets(String inflix){
-        StringBuilder infixAdjusted = new StringBuilder();
+    protected static String convertToNormalBrackets(String infix){
+        StringBuilder normalizedInflix = new StringBuilder(); // string top store the generalized formula
         
-        for(char c: inflix.toCharArray()){
-            switch (c) {
-                case '[', '{' -> infixAdjusted.append('(');
-                case ']', '}' -> infixAdjusted.append(')');
-                default -> infixAdjusted.append(c);
+        for (char c: infix.toCharArray()){
+            switch(c){
+                case '[','{' -> normalizedInflix.append('(');
+                case ']','}' -> normalizedInflix.append(')');
+                default -> normalizedInflix.append(c); // non bracket type charcters are added to the string statement without any change
             }
         }
-
-        return infixAdjusted.toString();     
+        return normalizedInflix.toString();
     }
 }
