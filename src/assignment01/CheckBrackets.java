9b9496d5c341eb41fd6108a8345d98d4e7daf2ba
Melkor <127069062+WWI2196@users.noreply.github.com>
2024-10-13 20:44:51 +0530

    Update CheckBrackets.java

--- src/assignment01/CheckBrackets.java
+++ src/assignment01/CheckBrackets.java
@@ -4,26 +4,28 @@ import java.util.*;
 
 public class CheckBrackets {
     
-    protected static boolean areBracketBalanced(String formula){
+   protected static boolean areBracketsBalanced(String formula){
         Stack<Character> bracketStack = new Stack<>();
         
-        for(char c: formula.toCharArray() ){
-            if (c == '(' || c == '[' || c == '{'){
+        for (char c:formula.toCharArray()){
+            if (c == '('||c == '['||c == '{'){ // for open brackets, push them to the stack
                 bracketStack.push(c);
             }
-            else if ( c == ')' || c == ']' || c == '}'){
-                if (bracketStack.empty()){
+            
+            if (c == ')'||c == ']'||c == '}'){ // if close brackets found, check whether the current stack element is the corresponding open bracket
+                if(bracketStack.empty()){
                     return false;
                 }
                 
                 char c_1 = bracketStack.pop();
-                if(!((c_1 == '(' && c == ')')||
-                   (c_1 == '[' && c == ']')||
-                   (c_1 == '{' && c == '}'))){
-                    return false;
+
+                if (!((c_1 == '(' && c == ')')||
+                    (c_1 == '[' && c == ']')||
+                    (c_1 == '{' && c == '}'))){
+                    return false; // if the stack element is not the corresponding open bracket, return false
                 }
             }
         }
-        return bracketStack.empty();
+        return bracketStack.empty(); // otherwise return true
     }
 }
